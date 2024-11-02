eb69b3acb27397c575a1204476fe9bd256648809
Josh Quintus <josh.quintus@gmail.com>
2024-11-01 20:16:44 -0400

    Getting the debugger to work

---
+++ packages/backend/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// src/index.ts
+const greeting = "Hello, TypeScript!";
+console.log(greeting);
+//packages/index.ts
--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,3 +1,5 @@
 // src/index.ts
 const greeting: string = "Hello, TypeScript!";
 console.log(greeting);
+
+//packages/index.ts
