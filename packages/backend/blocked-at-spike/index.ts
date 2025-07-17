010f234371e7033e78f84faf4090d4a012e44197
Josh Quintus <josh.quintus@hightouch.io>
2025-07-17 14:42:10 -0400

    Quickly set up blocked-at

--- packages/backend/blocked-at-spike/index.ts
+++ packages/backend/blocked-at-spike/index.ts
@@ -1,3 +1,11 @@
+import blockedAt from "blocked-at";
+
 export async function run() {
-  console.log("Hello, World!");
+  console.log("Starting blocked-at spike");
+
+  blockedAt(async () => {
+    console.log("Hello, World!");
+  });
+
+  console.log("> Blocked-at spike completed");
 }
